03a49719d4393fa3f5cda82a08a12c9032122195
J-Weeks <weeksjasons@gmail.com>
2015-02-06 13:59:03 -0500

    added a second pols routing unit test

--- spec/routing/pols_routing_spec.rb
+++ spec/routing/pols_routing_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe 'routes for pols' do
   it 'routes GET /pols to the pols controller' do
     expect(get('/pols')).to route_to('pols#index')
   end
+
+  it 'routes GET /pols/1 to the pols controller with id set to 1' do
+    expect(get('/pols/1')).to route_to(
+      controller: 'pols',
+      action: 'show',
+      id: '1'
+      )
+  end
 end
